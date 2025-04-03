c60a0d675e2a305543366d5393fd244a521430d0
sheben24 <zadorozhnaya.zlata2013@mail.ru>
2025-04-03 17:30:12 +0300

    commit

--- app/src/main/java/com/ahmadabuhasan/canvas/MainActivity.java
+++ app/src/main/java/com/ahmadabuhasan/canvas/MainActivity.java
@@ -2,85 +2,48 @@ package com.ahmadabuhasan.canvas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
-import static android.view.View.SYSTEM_UI_FLAG_FULLSCREEN;
-
-/**
- * CanvasExample shows how to create a custom view and draw on its canvas.
- */
+import com.skydoves.colorpickerview.ColorPickerDialog;
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
 
 
-public class DrawOriActivity extends AppCompatActivity {
-
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw_ori);
         LinearLayout linearLayout = new LinearLayout(this);
-        MyCanvasView myCanvasView;
-        EditText r = new EditText(this);
-        EditText g = new EditText(this);
-        EditText b = new EditText(this);
+
         Button color = new Button(this);
-        myCanvasView = new MyCanvasView(this);
+        MyCanvasView myCanvasView = new MyCanvasView(this);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
-        setContentView(linearLayout);
-        linearLayout.addView(r);
-        linearLayout.addView(g);
-        linearLayout.addView(b);
+
         linearLayout.addView(color);
         linearLayout.addView(myCanvasView);
-        r.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void afterTextChanged(Editable s) {
-                MyCanvasView.r = Integer.parseInt(String.valueOf(r.getText()));
-                MyCanvasView.g = Integer.parseInt(String.valueOf(r.getText()));
-                MyCanvasView.b = Integer.parseInt(String.valueOf(r.getText()));
-                color.setBackgroundColor(Color.rgb(MyCanvasView.r,MyCanvasView.g,MyCanvasView.b));
-            }
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-            }
-        });
 
+        setContentView(linearLayout);
 
-
-
-
+        new ColorPickerDialog.Builder(this)
+                .setTitle("Color picker")
+                .setPositiveButton("Confirm",
+                        (ColorEnvelopeListener) (envelope, fromUser) -> myCanvasView.setColor(envelope.getColor()))
+                .setNegativeButton("Cancel",
+                        (dialogInterface, i) -> dialogInterface.dismiss())
+                .attachAlphaSlideBar(false)
+                .attachBrightnessSlideBar(true)
+                .show();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.draw, menu);
-        return true;
-    }
-
-    @SuppressLint("WrongConstant")
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.edit_draw) {
-            startActivity(new Intent(this, DrawEditActivity.class));
-            Toast.makeText(DrawOriActivity.this, "Edit Draw on Canvas objects", 1000).show();
-        }
         return true;
     }
 }
--- app/src/main/java/com/ahmadabuhasan/canvas/MyCanvasView.java
+++ app/src/main/java/com/ahmadabuhasan/canvas/MyCanvasView.java
@@ -1,4 +1,7 @@
 package com.ahmadabuhasan.canvas;
+
+import androidx.core.content.res.ResourcesCompat;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -11,17 +14,19 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.content.res.Resources;
 
-/**
- * Custom view that follows touch events to draw on a canvas.
- */
-
 public class MyCanvasView extends View {
     private Paint mPaint;
-    public static int r = 255;
-    public static int g = 255;
-    public static int b = 255;
+    private Path mPath;
+    private int mDrawColor;
+    private int mBackgroundColor;
     private Canvas mExtraCanvas;
     private Bitmap mExtraBitmap;
+
+    public int r = 0;
+    public int g = 0;
+    public int b = 0;
+
+    private Rect mFrame;
 
     MyCanvasView(Context context) {
         this(context, null);
@@ -30,74 +35,111 @@ public class MyCanvasView extends View {
     public MyCanvasView(Context context, AttributeSet attributeSet) {
         super(context);
 
+        mBackgroundColor = ResourcesCompat.getColor(getResources(),
+                R.color.opaque_orange, null);
+        mDrawColor = ResourcesCompat.getColor(getResources(),
+                R.color.opaque_yellow, null);
 
+        // Holds the path we are currently drawing.
+        mPath = new Path();
+        // Set up the paint with which to draw.
         mPaint = new Paint();
+
+        System.out.println(mDrawColor);
+
         mPaint.setColor(Color.rgb(r, g, b));
+
         mPaint.setAntiAlias(true);
         mPaint.setDither(true);
-        mPaint.setStyle(Paint.Style.FILL); // default: FILL
+        mPaint.setStyle(Paint.Style.STROKE); // default: FILL
         mPaint.setStrokeJoin(Paint.Join.ROUND); // default: MITER
         mPaint.setStrokeCap(Paint.Cap.ROUND); // default: BUTT
         mPaint.setStrokeWidth(12); // default: Hairline-width (really thin)
     }
 
+    /**
+     * Note: Called whenever the view changes size.
+     * Since the view starts out with no size, this is also called after
+     * the view has been inflated and has a valid size.
+     */
     @Override
     protected void onSizeChanged(int width, int height,
                                  int oldWidth, int oldHeight) {
         super.onSizeChanged(width, height, oldWidth, oldHeight);
+        // Create bitmap, create canvas with bitmap, fill canvas with color.
         mExtraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         mExtraCanvas = new Canvas(mExtraBitmap);
-        mExtraCanvas.drawColor(Color.rgb(255,255,255));
-        int inset = 40;
+        mExtraCanvas.drawColor(mBackgroundColor);
 
+        // Calculate the rect a frame around the picture.
+        int inset = 40;
+        mFrame = new Rect(inset, inset, width - inset, height - inset);
     }
-    private static final float TOUCH_TOLERANCE = 4;
+
+    public void setColor(int color) {
+        mPaint.setColor(color);
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        // Draw the bitmap that has the saved path.
         canvas.drawBitmap(mExtraBitmap, 0, 0, null);
 
-
+        // Draw a frame around the picture.
+        canvas.drawRect(mFrame, mPaint);
     }
+
+    // Variables for the latest x,y values,
+    // which are the starting point for the next path.
     private float mX, mY;
+    // Don't draw every single pixel.
+    // If the finger has has moved less than this distance, don't draw.
+    private static final float TOUCH_TOLERANCE = 4;
+
+    // The following methods factor out what happens for different touch events,
+    // as determined by the onTouchEvent() switch statement.
+    // This keeps the switch statement
+    // concise and and easier to change what happens for each event.
+
     private void touchStart(float x, float y) {
+        mPath.moveTo(x, y);
         mX = x;
         mY = y;
-        int left = (int) x / 50 * 50 ;
-        int right = ((int) x / 50 * 50) + 50;
-        int top = (int) y / 50 * 50 ;
-        int bottom = ((int) y / 50 * 50) + 50 ;
-        mExtraCanvas.drawRect(left, top, right, bottom, mPaint);
-
-
     }
+
     private void touchMove(float x, float y) {
         float dx = Math.abs(x - mX);
         float dy = Math.abs(y - mY);
         if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
-            int left = (int) x / 50 * 50 ;
-            int right = ((int) x / 50 * 50) + 50;
-            int top = (int) y / 50 * 50 ;
-            int bottom = ((int) y / 50 * 50) + 50 ;
-            mExtraCanvas.drawRect(left, top, right, bottom, mPaint);
+            // QuadTo() adds a quadratic bezier from the last point,
+            // approaching control point (x1,y1), and ending at (x2,y2).
+            mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+            mX = x;
+            mY = y;
+            // Draw the path in the extra bitmap to save it.
+            mExtraCanvas.drawPath(mPath, mPaint);
         }
     }
 
-
-
     private void touchUp() {
+        // Reset the path so it doesn't get drawn again.
+        mPath.reset();
     }
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         float x = event.getX();
         float y = event.getY();
 
+        // Invalidate() is inside the case statements because there are many
+        // other types of motion events passed into this listener,
+        // and we don't want to invalidate the view for those.
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 touchStart(x, y);
-                invalidate();
+                // No need to invalidate because we are not drawing anything.
                 break;
             case MotionEvent.ACTION_MOVE:
                 touchMove(x, y);
@@ -107,6 +149,7 @@ public class MyCanvasView extends View {
                 touchUp();
                 break;
             default:
+                // do nothing
         }
         return true;
     }
@@ -114,6 +157,8 @@ public class MyCanvasView extends View {
     public static int getScreenWidth() {
         return Resources.getSystem().getDisplayMetrics().widthPixels;
     }
+
     public static int getScreenHeight() {
         return Resources.getSystem().getDisplayMetrics().heightPixels;
-    }}
+    }
+}
